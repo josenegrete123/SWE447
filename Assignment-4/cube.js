aa1648c2ce01935ed2b2ac8852316102d284987b
josenegrete123 <35516746+josenegrete123@users.noreply.github.com>
2018-02-08 19:50:39 -0800

    Update cube.js

--- Assignment-4/cube.js
+++ Assignment-4/cube.js
@@ -24,7 +24,7 @@ function init() {
 function render() {
   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 
-  angle += 2.0; // degrees
+  angle += 3.0; // degrees
 
   cube.MV = rotate( angle, [1, 1, 0] );
 
