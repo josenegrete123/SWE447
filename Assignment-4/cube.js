6121ee1f30e56865ffc237c00e8cd8bec3d3b667
josenegrete123 <35516746+josenegrete123@users.noreply.github.com>
2018-02-13 20:24:59 -0800

    Update cube.js

--- Assignment-4/cube.js
+++ Assignment-4/cube.js
@@ -18,6 +18,10 @@ function init() {
   cube = new Cube();
 
   render();
+  
+  document.getElementById("xButton").onclick = function(){
+    consol.log("someone pressed this button");
+  }
 }
 
 function render() {
