79565c9b89ee979ba403cd52d87815adb2c9d824
josenegrete123 <35516746+josenegrete123@users.noreply.github.com>
2018-03-01 13:29:40 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -1,6 +1,26 @@
 var cone = null;
 var gl = null;
 
+var near = 1.0;     // near clipping plane's distance
+var far = 10.0;     // far clipping plane's distance
+
+// Viewing transformation parameters
+var V = undefined;
+var M = undefined;
+var angle = 0.0;
+var dAngle = 0.0; //Math.PI / 10.0;
+var S = undefined;
+var mouseDown = false;
+var lastMouseX = null;
+var lastMouseY = null;
+var zvalue = -0.5*(near + far);
+var offset = [ 0.0,  0.0, 0.0 ];
+var rotationAxis = undefined;
+var xAxis = [1, 0, 0];
+var yAxis = [0, 1, 0];
+var speed = 1;
+var stoprotating = 0;
+
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
     
@@ -17,6 +37,33 @@ function init() {
     
     render();
 }
+
+canvas.onmousedown = function handleMouseDown(event) {
+        mouseDown = true;
+        lastMouseX = event.clientX;
+        lastMouseY = event.clientY;
+    }
+
+document.onmouseup = function handleMouseUp(event) {
+        mouseDown = false;
+        if (stoprotating) dAngle = 0.0;
+        return;
+
+    }
+document.onmousemove = function handleMouseMove(event) {
+    if (!mouseDown) {
+      if(stoprotating) dAngle = 0.0;
+      return;
+    }
+    var newX = event.clientX;
+    var newY = event.clientY;
+
+    var deltaX = newX - lastMouseX;
+    var deltaY = newY - lastMouseY;
+    dAngle = degToRad(deltaX + deltaY) * Math.PI * 5;
+    lastMouseX = newX;
+    lastMouseY = newY;
+    }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
