c3752bb10b7bacf26e732f0f017b88d0003ce055
josenegrete123 <35516746+josenegrete123@users.noreply.github.com>
2018-03-02 18:24:48 -0800

    Update Cone.js

--- Common/Cone.js
+++ Common/Cone.js
@@ -30,13 +30,13 @@ function Cone( gl, numSides, vertexShaderId, fragmentShaderId ) {
     for ( var i = 0; i < n; ++i ) {
         theta = i * dTheta;
         positions.push( Math.cos(theta), Math.sin(theta), 4.0 );
-        colors.push(1.0, 0.0, 0.0);
+        colors.push(1.0, 0.0, 1.0);
         
         indices.push(n - i);
     }
     // Size of CONE/ Stretch/Color
     positions.push( 0.0, 0.0, 0.0 );
-    colors.push(1.0, 0.0, 1.0);
+    colors.push(1.0, 0.0, 0.0);
     
     
     indices.push(n);
